a7c5b5f8f0619eae464121cd02ac2d56ce07e2c9
Ginson Pallickal <ginsonpallickal@gmail.com>
2023-08-11 20:42:08 +0530

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,44 +1,67 @@
 import mediapipe as mp
 import cv2
 from pynput.mouse import Button, Controller
+import time
 
-mp_drawing= mp.solutions.drawing_utils
-mp_drawing_styles= mp.solutions.drawing_styles
-mp_hands=mp.solutions.hands
-mouse=Controller()
+# Initialize mediapipe and mouse controller
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+mouse = Controller()
 
+# Initialize webcam
+cap = cv2.VideoCapture(0)
 
-cap=cv2.VideoCapture(0)
-hands=mp_hands.Hands()
-cv2.namedWindow("Hands", cv2.WINDOW_NORMAL)
-c=0
+# Initialize mediapipe hands and create a named window
+hands = mp_hands.Hands()
+#cv2.namedWindow("Hands", cv2.WINDOW_NORMAL)
 
 while True:
-    data, img= cap.read()
-    image=cv2.cvtColor(cv2.flip(img,1),cv2.COLOR_BGR2RGB)
-    result=hands.process(image)
-    
-    image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    # Read a frame from the webcam
+    ret, img = cap.read()
+    if not ret:
+        break
+
+    # Flip the image horizontally for a mirrored view
+    image = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB)
+
+    # Process the hand landmarks using mediapipe
+    result = hands.process(image)
+
+    # Convert the image back to BGR format for OpenCV
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     if result.multi_hand_landmarks:
         for hand_landmarks in result.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(image,hand_landmarks,mp_hands.HAND_CONNECTIONS)
-            x=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x)
-            y=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y)
-            x1=(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x)
-            y1=(hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y) 
-            if x*100>25 and x*100<75 and y*100<50:
-                x=(x-0.25)/(0.50)
-                print(x) 
-                wristx=(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x)
-                wristy=(hand_landmarks.landmap--
-                
-                rk[mp_hands.HandLandmark.WRIST].y)
-                mouse.position = (int(1280*x),int(800*y))
-                if abs((x-x1)*100)<=5 and abs((y-y1)*100)<=5 and abs((wristy-y1)*100)>50:
-                    print(c,"mouse click")
-                    #mouse.click(Button.left,1)
-                #print(abs((x-x1)*100), abs((y-y1)*100))
+            # Draw landmarks and connections on the image
+            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            
+            # Extract hand landmark coordinates
+            x = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x
+            y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
+            x1 = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x
+            y1 = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y
 
-    cv2.resizeWindow("Hands", 1280, 800)        
-    cv2.imshow("Hands", image)
-    cv2.waitKey(1)
+            # Check if index finger is in a certain region
+            if 0.25 < x < 0.75 and y < 0.5:
+                x_normalized = (x - 0.25) / 0.5
+                
+                # Move the mouse cursor
+                mouse.position = (int(1280 * x_normalized), int(800 * y))
+                
+                # Check for a mouse click gesture
+                if abs((x - x1) * 100) <= 5 and abs((y - y1) * 100) <= 5:
+                    print("Mouse click")
+                    mouse.click(Button.left, 2)
+                    time.sleep(2)
+            
+    # Resize and display the image
+    #cv2.resizeWindow("Hands", 1280, 800)
+    #cv2.imshow("Hands", image)
+    
+    # Exit the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources and close windows
+cap.release()
+cv2.destroyAllWindows()
